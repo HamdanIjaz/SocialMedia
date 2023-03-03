dbc1438b35f74f14a10e6dd9c0b9eaee75799987
Ghulam Mohayyuddin <87536847+mohayudin@users.noreply.github.com>
2023-03-04 02:52:51 +0500

    End of chapter 4

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
-
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,6 +24,8 @@ if (app.Environment.IsDevelopment())
 
 //app.UseAuthorization();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
